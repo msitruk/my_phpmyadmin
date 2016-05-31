8d54a4a3ea75aad41cf8a8c4b03cdbebf818c6eb
SFatier <fatier-segolene@outlook.com>
2016-05-31 13:30:15 +0200

    add function delete

--- php/controller/table.php
+++ php/controller/table.php
@@ -16,7 +16,40 @@ function list_table($connection, $table)
 	return ($bases);
 }
 
-function insert_into($connection, $table)
-{
+function delete_database($table, $connection){
 	
+	$sql = "DROP TABLE $table";
+	try 
+	{
+	  $connection->query($sql);
+      echo "Database deleted successfully<br>";
+	}
+	catch (PDOException $e)
+	{
+		echo "Delete Database failed";
+	}
 }
+
+function nb_column ($connection, $db,  $table)
+{
+
+	$i = 0;
+	$sql = "select * from information_schema.columns where table_schema = '"."$db"."' and table_name= '"."$table"."' ";
+	try
+	{
+		$req = $connection->query($sql);
+	}
+	catch (PDOException $e)
+	{
+		echo "Show table of database failed";
+	}
+	while ($row = $req->fetch())
+	{
+			$tables["table".$i] = $row[0];
+			$i = $i + 1;
+	}
+	$req->closeCursor();
+	echo $i;
+	return ($i);
+}
+
