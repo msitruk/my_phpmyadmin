beaefb40fd2a855bad55834654f1cc94d0119e4c
msitruk <maxime.sitruk@gmail.com>
2016-06-06 16:21:35 +0200

    Update connection.php

--- php/models/connection.php
+++ php/models/connection.php
@@ -2,7 +2,7 @@
 function connection (){
 	try
 {
-	$connection = new PDO('mysql:host=localhost;dbname=information_schema', 'root', 'pf69ppyo');
+	$connection = new PDO('mysql:host=localhost;dbname=information_schema', 'root', 'XXXX');
 }
 catch (Exception $e)
 {
